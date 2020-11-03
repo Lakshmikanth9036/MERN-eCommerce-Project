9f9432d258b7ce6b58db2a00d306e55417b01573
Lakshmikanth9036 <kanth9036.1997@gmail.com>
2020-11-03 10:32:19 +0530

    implemented front end user authentication and user profile update

---
+++ frontend/src/store/actions/userActions.js
@@ -0,0 +1,147 @@
+import * as actionType from "./actionTypes";
+import axios from "axios";
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: actionType.USER_LOGIN_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "/proshop/users/login",
+      {
+        email,
+        password,
+      },
+      config
+    );
+    dispatch({
+      type: actionType.USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (err) {
+    dispatch({
+      type: actionType.USER_LOGIN_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({ type: actionType.USER_LOGOUT });
+};
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: actionType.USER_REGISTER_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "/proshop/users",
+      {
+        name,
+        email,
+        password,
+      },
+      config
+    );
+    dispatch({
+      type: actionType.USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+    dispatch({
+      type: actionType.USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (err) {
+    dispatch({
+      type: actionType.USER_REGISTER_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: actionType.USER_DETAILS_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.get(`/proshop/users/${id}`, config);
+    dispatch({
+      type: actionType.USER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: actionType.USER_DETAILS_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
+
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: actionType.USER_UPDATE_PROFILE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.put(`/proshop/users/profile`, user, config);
+    dispatch({
+      type: actionType.USER_UPDATE_PROFILE_SUCCESS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: actionType.USER_UPDATE_PROFILE_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
---
+++ frontend/src/store/reducers/userReducers.js
@@ -0,0 +1,55 @@
+import * as actionType from "../actions/actionTypes";
+
+export const userLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case actionType.USER_LOGIN_REQUEST:
+      return { loading: true };
+    case actionType.USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case actionType.USER_LOGIN_FAIL:
+      return { loading: false, error: action.payload };
+    case actionType.USER_LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case actionType.USER_REGISTER_REQUEST:
+      return { loading: true };
+    case actionType.USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case actionType.USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case actionType.USER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case actionType.USER_DETAILS_SUCCESS:
+      return { loading: false, user: action.payload };
+    case actionType.USER_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+  switch (action.type) {
+    case actionType.USER_UPDATE_PROFILE_REQUEST:
+      return { loading: true };
+    case actionType.USER_UPDATE_PROFILE_SUCCESS:
+      return { loading: false, success: true ,userInfo: action.payload };
+    case actionType.USER_UPDATE_PROFILE_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
